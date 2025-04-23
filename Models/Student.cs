781f7d14e1619a38b1daaf8d9f14fb6e5955d868
Md Abdus Salam <67725823+massofteng@users.noreply.github.com>
2025-04-23 18:59:13 +0600

    change bson

--- Models/Student.cs
+++ Models/Student.cs
@@ -12,11 +12,13 @@ namespace CoreDotNetToken.Models
 
         [Required(ErrorMessage = "Name is required")]
         [MinLength(2, ErrorMessage = "Name must be at least 2 characters long")]
+        [BsonElement("name")]
         public string name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Roll is required")]
         [RegularExpression(@"^\d+$", ErrorMessage = "Roll must be numeric")]
-        public string roll { get; set; } = string.Empty;
+        [BsonElement("roll")]
+        public int roll { get; set; }
     }
 }
 
